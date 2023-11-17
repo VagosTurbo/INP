c8eac881036807d410272d4d80a385f6b27dbf45
VagosTurbo <semanco.boris@gmail.com>
2023-11-17 01:55:09 +0100

    optimized

--- proj2/xseman06.s
+++ proj2/xseman06.s
@@ -1,8 +1,8 @@
 ; Autor reseni: Boris Semanco - xseman06
-; Pocet cyklu k serazeni puvodniho retezce: 3705
-; Pocet cyklu razeni sestupne serazeneho retezce: 3734
-; Pocet cyklu razeni vzestupne serazeneho retezce: 3649
-; Pocet cyklu razeni retezce s vasim loginem: 824
+; Pocet cyklu k serazeni puvodniho retezce: 2681
+; Pocet cyklu razeni sestupne serazeneho retezce: 2710
+; Pocet cyklu razeni vzestupne serazeneho retezce: 2625
+; Pocet cyklu razeni retezce s vasim loginem: 610
 ; Implementovany radici algoritmus: Bubble Sort
 ; ------------------------------------------------
 
@@ -23,60 +23,58 @@ params_sys5:    .space  8   ; misto pro ulozeni adresy pocatku
 
 main:
     daddi r4, r0, login
-    daddi r2, r0, 0 ; n = 0
-    daddi r15, r0, 1 ; just for n - 1
-    ; get arr size and sort it
-    jal arr_size
+    daddi r2, r0, 0         ; n = 0
+    
+    jal arr_size            ; get arr size and sort it
 
-    daddi r4, r0, login
-    jal print_string
+    daddi r4, r0, login     ; reset arr pointer
+    jal print_string        ; print sorted arr
 
-    syscall 0   ; halt
+    syscall 0
 
 arr_size:
-    lbu r5, 0(r4)   ; arr[i]
-    beqz r5, bubble_sort   ; if arr[i] == '\0' then done
-    daddi r2, r2, 1 ; n++
-    daddi r4, r4, 1 ; moves arr pointer to next char
-    j arr_size ; loop
+    lbu r5, 0(r4)           ; arr[i]
+    beqz r5, bubble_sort    ; if arr[i] == '\0' then start bubble sort
+    daddi r2, r2, 1         ; n++
+    daddi r4, r4, 1         ; moves arr pointer to next char
+    j arr_size              ; loop
 
 bubble_sort:
-    daddi r4, r0, login ; reset arr pointer
-    daddi r1, r0, 0 ; i = 0
+    daddi r4, r0, login     ; reset arr pointer
+    daddi r1, r0, 0         ; i = 0
 
 outer_loop:
-    dsubu r6, r2, r15 ; n - 1, r6 = n - 1
-    slt r7, r1, r6 ; i < n - 1, r7 = 1 if true
-    beqz r7, done_sorting ; if i >= n - 1 then done
+    daddi r6, r2, -1        ; n - 1, r6 = n - 1
+    beq r1, r6, done_sorting ; if i == n - 1, sorting complete
 
-    daddi r3, r0, 0 ; j = 0
+    dsubu r8, r6, r1        ; r8 = n - 1 - i
+
+    daddi r3, r0, 0         ; j = 0
     inner_loop:
 
-        dsubu r8, r6, r1 ; r8 = n - i - 1 
-        slt r9, r3, r8 ; j < n - i - 1, r9 = 1 if true
-        beqz r9, outer_done ; if j >= n - i - 1 then inner_done
+        beq r3, r8, outer_done ; if j == n - i - 1, move to outer loop
 
-        lbu r10, 0(r4) ; arr[j]
-        lbu r11, 1(r4) ; arr[j + 1]
+        lbu r10, 0(r4)      ; arr[j]
+        lbu r11, 1(r4)      ; arr[j + 1]
 
-        slt r12, r10, r11 ; arr[j] < arr[j + 1], r12 = 1 if true
-        bnez r12, inner_done ; if arr[j] >= arr[j + 1] then inner_loop_end
+        slt r12, r10, r11   ; arr[j] < arr[j + 1], r12 = 1 if true
+        bnez r12, inner_done ; if arr[j] >= arr[j + 1] then inner_done
 
-        sb r11, 0(r4) ; arr[j] = arr[j + 1]
-        sb r10, 1(r4) ; arr[j + 1] = arr[j]
+        sb r11, 0(r4)       ; arr[j] = arr[j + 1]
+        sb r10, 1(r4)       ; arr[j + 1] = arr[j]
  
     inner_done:
-        daddi r3, r3, 1 ; j++
-        daddi r4, r4, 1 ; moves arr pointer to next char
-        j inner_loop ; loop
+        daddi r3, r3, 1     ; j++
+        daddi r4, r4, 1     ; moves arr pointer to next char
+        j inner_loop        ; loop
 
 outer_done:
-    daddi r1, r1, 1 ; i++
-    daddi r4, r0, login ; reset arr pointer
-    j outer_loop ; loop
+    daddi r1, r1, 1         ; i++
+    daddi r4, r0, login     ; reset arr pointer
+    j outer_loop            ; loop
 
 done_sorting:
-    jr r31 ; return
+    jr r31                  ; return from main
 
 
 
